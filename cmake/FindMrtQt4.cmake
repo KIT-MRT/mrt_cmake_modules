ee2e875d12e42f9ddccef3a75f2a01f365dccf82
Johannes Beck <johannes.beck@kit.edu>
2015-11-18 07:42:36 +0100

    Fixed bug in MRT Qt4 find script

--- cmake/FindMrtQt4.cmake
+++ cmake/FindMrtQt4.cmake
@@ -1,2 +1,4 @@
-find_package(Qt4 REQUIRED ${Qt4_FIND_COMPONENTS})
+find_package(Qt4 REQUIRED ${MrtQt4_FIND_COMPONENTS})
+message( STATUS "Qt Components: " ${MrtQt4_FIND_COMPONENTS})
 include(${QT_USE_FILE})
+
