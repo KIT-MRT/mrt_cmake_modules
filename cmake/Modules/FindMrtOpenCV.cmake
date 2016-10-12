f9199acfe9db3180798e4c68fd949edd9d6b4fb0
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2016-10-12 16:07:34 +0200

    gna

--- cmake/Modules/FindMrtOpenCV.cmake
+++ cmake/Modules/FindMrtOpenCV.cmake
@@ -3,7 +3,7 @@
 set(PACKAGE_LOCAL_PATH "/mrtsoftware/pkg/local/opencv-2.4.11/share/OpenCV")
 set(PACKAGE_PATH "/mrtsoftware/pkg/opencv-2.4.11/share/OpenCV")
 
-# old mrtsoftware-style
+#old mrtsoftware-style
 if (EXISTS ${PACKAGE_LOCAL_PATH})
 	set(OpenCV_DIR ${PACKAGE_LOCAL_PATH})
 elseif(EXISTS ${PACKAGE_PATH})
