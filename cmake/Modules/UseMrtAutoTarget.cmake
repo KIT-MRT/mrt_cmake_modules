e0a4296ac835b85109acd422c644d0c9eca409cd
Niels Ole Salscheider <ole.salscheider@kit.edu>
2016-11-18 13:36:02 +0100

    Whitespace fixes
    Let's see if the post-receive hook for Github works...

--- cmake/Modules/UseMrtAutoTarget.cmake
+++ cmake/Modules/UseMrtAutoTarget.cmake
@@ -4,13 +4,13 @@ function(add_executable_auto TARGET_NAME)
         #Given file list empty. Generate a dummy file and use this as target.
         #Also warn the user
         set(_AUTO_GEN_FILE_NAME_ "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/${TARGET_NAME}_exec_dummy.cpp")
-        
+
         file(GENERATE OUTPUT "${_AUTO_GEN_FILE_NAME_}" CONTENT "int main()\n{\n\t#warning \"Used auto generated file for target\"\n}")
         set_source_files_properties("${_AUTO_GEN_FILE_NAME_}" PROPERTIES GENERATED TRUE)
         list(APPEND FILE_LIST "${_AUTO_GEN_FILE_NAME_}")
         message(WARNING "No files specified for target ${TARGET_NAME}.")
     endif()
-    
+
     add_executable(${TARGET_NAME} ${FILE_LIST})
 endfunction()
 
@@ -25,43 +25,42 @@ function(add_library_auto TARGET_NAME)
         list(APPEND FILE_LIST "${_AUTO_GEN_FILE_NAME_}")
         message(WARNING "No files specified for target ${TARGET_NAME}.")
     endif()
-    
+
     add_library(${TARGET_NAME} ${FILE_LIST})
 endfunction()
 
 #search for subfolders in src
 function(glob_folders DIRECTORY_LIST SEARCH_DIRECTORY)
-	execute_process(COMMAND find . -mindepth 1 -type d WORKING_DIRECTORY "${SEARCH_DIRECTORY}" OUTPUT_VARIABLE DIRECTORIES)
-	
-	set(_DIRECTORY_LIST_ "")
-	if (NOT DIRECTORIES STREQUAL "")
-		string(REPLACE "\n" ";" DIRECTORIES ${DIRECTORIES})
-	
-		foreach(SRC_DIR ${DIRECTORIES})
-			#remove trailing "./"
-			string(SUBSTRING "${SRC_DIR}" 2 -1 EXEC_NAME)
-			list(APPEND _DIRECTORY_LIST_ ${EXEC_NAME})
-		endforeach()
-		
-		set(${DIRECTORY_LIST} ${_DIRECTORY_LIST_} PARENT_SCOPE)
-	endif()
+    execute_process(COMMAND find . -mindepth 1 -type d WORKING_DIRECTORY "${SEARCH_DIRECTORY}" OUTPUT_VARIABLE DIRECTORIES)
+
+    set(_DIRECTORY_LIST_ "")
+    if (NOT DIRECTORIES STREQUAL "")
+        string(REPLACE "\n" ";" DIRECTORIES ${DIRECTORIES})
+
+        foreach(SRC_DIR ${DIRECTORIES})
+            #remove trailing "./"
+            string(SUBSTRING "${SRC_DIR}" 2 -1 EXEC_NAME)
+            list(APPEND _DIRECTORY_LIST_ ${EXEC_NAME})
+        endforeach()
+
+        set(${DIRECTORY_LIST} ${_DIRECTORY_LIST_} PARENT_SCOPE)
+    endif()
 endfunction()
 
 macro(glob_ros_files excecutable_name extension_name)
-	file(GLOB ROS_${excecutable_name}_FILES RELATIVE "${CMAKE_CURRENT_LIST_DIR}/${extension_name}" "${extension_name}/*.${extension_name}")
-	
-	if (ROS_${excecutable_name}_FILES)
-		#work around to execute a command wich name is given in a variable
-		#write a file with the command, include it and delete the file again
-		file(WRITE "${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/_GLOB_ROS_TEMP_FILE.cmake" "${excecutable_name}(
-			DIRECTORY ${extension_name}
-			FILES
-			${ROS_${excecutable_name}_FILES}
-		)")
-		include("${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/_GLOB_ROS_TEMP_FILE.cmake")
-		file(REMOVE "${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/_GLOB_ROS_TEMP_FILE.cmake")
-		
-		set(ROS_GENERATE_MESSAGES True)
-	endif()
-endmacro()
+    file(GLOB ROS_${excecutable_name}_FILES RELATIVE "${CMAKE_CURRENT_LIST_DIR}/${extension_name}" "${extension_name}/*.${extension_name}")
 
+    if (ROS_${excecutable_name}_FILES)
+        #work around to execute a command wich name is given in a variable
+        #write a file with the command, include it and delete the file again
+        file(WRITE "${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/_GLOB_ROS_TEMP_FILE.cmake" "${excecutable_name}(
+            DIRECTORY ${extension_name}
+            FILES
+            ${ROS_${excecutable_name}_FILES}
+        )")
+        include("${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/_GLOB_ROS_TEMP_FILE.cmake")
+    file(REMOVE "${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/_GLOB_ROS_TEMP_FILE.cmake")
+
+    set(ROS_GENERATE_MESSAGES True)
+    endif()
+endmacro()
