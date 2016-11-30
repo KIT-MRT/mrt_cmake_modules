c30c76d7d90773cb9c0f093055c0a33a84d9d883
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2016-11-30 11:22:02 +0100

    added leptonica and tesseract findscripts for xenial

--- cmake/Modules/FindMrtLeptonica.cmake
+++ cmake/Modules/FindMrtLeptonica.cmake
@@ -3,8 +3,8 @@ set(PACKAGE_LIBRARIES lept)
 set(PACKAGE_LOCAL_PATH "/mrtsoftware/pkg/local/leptonica-1.72")
 set(PACKAGE_PATH "/mrtsoftware/pkg/leptonica-1.72")
 
-find_path(Leptonica_INCLUDE_DIR NAMES ${PACKAGE_HEADER_FILES} PATHS "${PACKAGE_LOCAL_PATH}/include" "${PACKAGE_PATH}/include")
-find_library(Leptonica_LIBRARIES NAMES ${PACKAGE_LIBRARIES} PATHS "${PACKAGE_LOCAL_PATH}/lib" "${PACKAGE_PATH}/lib")
+find_path(Leptonica_INCLUDE_DIR NAMES ${PACKAGE_HEADER_FILES} PATHS "${PACKAGE_LOCAL_PATH}/include" "${PACKAGE_PATH}/include" /usr/local/include /usr/include)
+find_library(Leptonica_LIBRARIES NAMES ${PACKAGE_LIBRARIES} PATHS "${PACKAGE_LOCAL_PATH}/lib" "${PACKAGE_PATH}/lib" /usr/local/lib /usr/lib)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Leptonica FOUND_VAR Leptonica_FOUND REQUIRED_VARS Leptonica_INCLUDE_DIR Leptonica_LIBRARIES)
--- cmake/Modules/FindMrtTesseract.cmake
+++ cmake/Modules/FindMrtTesseract.cmake
@@ -3,8 +3,8 @@ set(PACKAGE_LIBRARIES tesseract)
 set(PACKAGE_LOCAL_PATH "/mrtsoftware/pkg/local/tesseract-3.04.00")
 set(PACKAGE_PATH "/mrtsoftware/pkg/tesseract-3.04.00")
 
-find_path(Tesseract_INCLUDE_DIR NAMES ${PACKAGE_HEADER_FILES} PATHS "${PACKAGE_LOCAL_PATH}/include" "${PACKAGE_PATH}/include")
-find_library(Tesseract_LIBRARIES NAMES ${PACKAGE_LIBRARIES} PATHS "${PACKAGE_LOCAL_PATH}/lib" "${PACKAGE_PATH}/lib")
+find_path(Tesseract_INCLUDE_DIR NAMES ${PACKAGE_HEADER_FILES} PATHS "${PACKAGE_LOCAL_PATH}/include" "${PACKAGE_PATH}/include" /usr/local/include /usr/include)
+find_library(Tesseract_LIBRARIES NAMES ${PACKAGE_LIBRARIES} PATHS "${PACKAGE_LOCAL_PATH}/lib" "${PACKAGE_PATH}/lib" /usr/local/lib /usr/lib)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Tesseract FOUND_VAR Tesseract_FOUND REQUIRED_VARS Tesseract_INCLUDE_DIR Tesseract_LIBRARIES)
