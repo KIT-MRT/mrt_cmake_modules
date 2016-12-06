addc0a88ca83b942c20cd36493a1f17eced0688a
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2016-12-06 16:58:15 +0100

    ubuntu 14 now uses libmetis instead of mrtMetis

--- cmake/Modules/FindMrtMetis.cmake
+++ cmake/Modules/FindMrtMetis.cmake
@@ -1,10 +1,8 @@
 set(PACKAGE_HEADER_FILES metis.h)
 set(PACKAGE_LIBRARIES metis)
-set(PACKAGE_LOCAL_PATH "/mrtsoftware/pkg/local/metis-4.0.3")
-set(PACKAGE_PATH "/mrtsoftware/pkg/metis-4.0.3")
 
-find_path(Metis_INCLUDE_DIR NAMES ${PACKAGE_HEADER_FILES} PATHS "${PACKAGE_LOCAL_PATH}/include" "${PACKAGE_PATH}/include")
-find_library(Metis_LIBRARIES NAMES ${PACKAGE_LIBRARIES} PATHS "${PACKAGE_LOCAL_PATH}/lib" "${PACKAGE_PATH}/lib")
+find_path(Metis_INCLUDE_DIR NAMES ${PACKAGE_HEADER_FILES})
+find_library(Metis_LIBRARIES NAMES ${PACKAGE_LIBRARIES})
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Metis FOUND_VAR Metis_FOUND REQUIRED_VARS Metis_INCLUDE_DIR Metis_LIBRARIES)
