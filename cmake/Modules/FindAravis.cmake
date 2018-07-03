7d7ccc27205ae81788be463cfc32077c23350e2f
Niels Ole Salscheider <ole.salscheider@kit.edu>
2018-07-03 17:23:49 +0200

    Fix for the Aravis find script

--- cmake/Modules/FindAravis.cmake
+++ cmake/Modules/FindAravis.cmake
@@ -1,5 +1,5 @@
 include(FindPkgConfig)
-pkg_check_modules(Aravis REQUIRED aravis)
+pkg_check_modules(Aravis REQUIRED aravis-0.6)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Aravis DEFAULT_MSG Aravis_LIBRARIES Aravis_INCLUDE_DIRS)
