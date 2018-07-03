aced9dae256afe885c9ecfee23a7969398e9359c
Niels Ole Salscheider <ole.salscheider@kit.edu>
2018-07-03 17:15:39 +0200

    Add Aravis cmake files

---
+++ cmake/Modules/FindAravis.cmake
@@ -0,0 +1,8 @@
+include(FindPkgConfig)
+pkg_check_modules(Aravis REQUIRED aravis)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Aravis DEFAULT_MSG Aravis_LIBRARIES Aravis_INCLUDE_DIRS)
+
+mark_as_advanced(Araivs_LIBRARIES Aravis_INCLUDE_DIRS)
+
