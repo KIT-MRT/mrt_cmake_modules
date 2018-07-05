6d30ae488fae4e2f990ca7a73f7f6629dc0ed260
Niels Ole Salscheider <ole.salscheider@kit.edu>
2018-07-05 17:10:41 +0200

    Update aravis find script

--- cmake/Modules/FindAravis.cmake
+++ cmake/Modules/FindAravis.cmake
@@ -1,8 +1,13 @@
+find_package(PkgConfig REQUIRED)
 include(FindPkgConfig)
-pkg_check_modules(Aravis REQUIRED aravis-0.6)
+pkg_check_modules(PC_ARAVIS REQUIRED aravis-0.6)
+
+set(Aravis_LIBRARIES ${PC_ARAVIS_LIBRARIES})
+set(Aravis_INCLUDE_DIRS ${PC_ARAVIS_INCLUDE_DIRS})
+set(Aravis_LIBRARY_DIRS ${PC_ARAVIS_LIBRARY_DIRS})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Aravis DEFAULT_MSG Aravis_LIBRARIES Aravis_INCLUDE_DIRS)
+find_package_handle_standard_args(Aravis DEFAULT_MSG PC_ARAVIS_LIBRARIES PC_ARAVIS_LIBRARY_DIRS PC_ARAVIS_INCLUDE_DIRS)
 
-mark_as_advanced(Araivs_LIBRARIES Aravis_INCLUDE_DIRS)
+mark_as_advanced(Aravis_LIBRARIES Aravis_INCLUDE_DIRS Aravis_LIBRARY_DIRS)
 
