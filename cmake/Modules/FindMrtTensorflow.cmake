c8af2e9892030c841da525a2c835ab3ed5949824
Niels Ole Salscheider <salscheider@fzi.de>
2019-07-22 14:59:28 +0200

    Update for new tensorflow release

--- cmake/Modules/FindMrtTensorflow.cmake
+++ cmake/Modules/FindMrtTensorflow.cmake
@@ -4,7 +4,12 @@ find_path(TENSORFLOW_INCLUDE_DIR tensorflow/core/public/session.h
   PATH_SUFFIXES tensorflow
   )
 
-set(TENSORFLOW_INCLUDE_DIR ${TENSORFLOW_INCLUDE_DIR})
+set(TENSORFLOW_INCLUDE_DIR
+    ${TENSORFLOW_INCLUDE_DIR}
+    ${TENSORFLOW_INCLUDE_DIR}/external/com_google_absl
+    ${TENSORFLOW_INCLUDE_DIR}/external/com_google_protobuf/src
+    ${TENSORFLOW_INCLUDE_DIR}/bazel-out/k8-opt/bin
+    )
 
 find_library(TENSORFLOW_CC_LIBRARY
   NAMES tensorflow_cc
@@ -12,17 +17,11 @@ find_library(TENSORFLOW_CC_LIBRARY
 find_library(TENSORFLOW_FRAMEWORK_LIBRARY
   NAMES tensorflow_framework
   )
-find_library(TENSORFLOW_TRT_LIBRARY
-  NAMES tftrt
-  )
 
 set(TENSORFLOW_LIBRARY ${TENSORFLOW_CC_LIBRARY})
 if(TENSORFLOW_FRAMEWORK_LIBRARY)
     set(TENSORFLOW_LIBRARY ${TENSORFLOW_LIBRARY} ${TENSORFLOW_FRAMEWORK_LIBRARY})
 endif(TENSORFLOW_FRAMEWORK_LIBRARY)
-if(TENSORFLOW_TRT_LIBRARY)
-    set(TENSORFLOW_LIBRARY ${TENSORFLOW_TRT_LIBRARY} ${TENSORFLOW_LIBRARY})
-endif(TENSORFLOW_TRT_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MrtTensorflow  DEFAULT_MSG
