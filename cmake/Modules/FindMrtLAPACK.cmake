ae4a9cadf87e14c7822aad939496dcd0ebd2c709
Johannes Beck <johannes.beck@kit.edu>
2019-12-03 12:58:01 +0100

    Fix find script for lapack when using cmake 3.16.

---
+++ cmake/Modules/FindMrtLAPACK.cmake
@@ -0,0 +1,13 @@
+if(MrtLAPACK_FIND_REQUIRED)
+	find_package(LAPACK QUIET REQUIRED)
+elseif(MrtLAPACK_FIND_QUIETLY)
+	find_package(LAPACK QUIET)
+else()
+	find_package(LAPACK QUIET)
+endif()
+
+# LAPACK_LIBRARIES are empty with cmake 3.16 but it is needed on ubuntu 18.04.
+# Maybe this is a bug in the LAPACK cmake find script.
+if (NOT LAPACK_LIBRARIES)
+    set(LAPACK_LIBRARIES "lapack")
+endif()
