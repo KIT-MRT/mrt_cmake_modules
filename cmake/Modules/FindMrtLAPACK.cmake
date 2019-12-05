3e01107fb431c22113891e317dab5170efa59a15
Johannes Beck <johannes.beck@kit.edu>
2019-12-05 15:06:51 +0100

    Added MrtLAPACK_FOUND variable

--- cmake/Modules/FindMrtLAPACK.cmake
+++ cmake/Modules/FindMrtLAPACK.cmake
@@ -6,6 +6,8 @@ else()
 	find_package(LAPACK QUIET)
 endif()
 
+set(MrtLAPACK_FOUND LAPACK_FOUND)
+
 # LAPACK_LIBRARIES are empty with cmake 3.16 but it is needed on ubuntu 18.04.
 # Maybe this is a bug in the LAPACK cmake find script.
 if (NOT LAPACK_LIBRARIES)
