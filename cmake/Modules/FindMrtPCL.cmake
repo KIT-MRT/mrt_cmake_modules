e3ca3aaf0701b8fac7c2453afea8fc63e10c8302
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2019-12-18 11:28:33 +0100

    Cleanup references to old /mrtsoftware directory

--- cmake/Modules/FindMrtPCL.cmake
+++ cmake/Modules/FindMrtPCL.cmake
@@ -1,18 +1,6 @@
-#Use the following block, if a package config file is already created by cmake.
-#Adjust the following paths if needed.
-set(PACKAGE_LOCAL_PATH "/mrtsoftware/pkg/local/pcl-1.8/share/pcl-1.8")
-set(PACKAGE_PATH "/mrtsoftware/pkg/pcl-1.8/share/pcl-1.8")
-
 cmake_policy(PUSH)
 if(POLICY CMP0074)
 	cmake_policy(SET CMP0074 NEW)
-endif()
-
-#old mrtsoftware-style
-if (EXISTS ${PACKAGE_LOCAL_PATH})
-	set(PCL_DIR ${PACKAGE_LOCAL_PATH})
-elseif(EXISTS ${PACKAGE_PATH})
-	set(PCL_DIR ${PACKAGE_PATH})
 endif()
 
 # find package component
