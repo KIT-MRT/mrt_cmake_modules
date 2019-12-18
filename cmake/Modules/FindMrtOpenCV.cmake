da3b3adad9dffe8ec57443b36e206771ac311991
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2019-12-18 12:50:15 +0100

    Adapt/cleanup our findscripts where targets are already defined by default findscripts

--- cmake/Modules/FindMrtOpenCV.cmake
+++
@@ -1,8 +0,0 @@
-# find package component
-if(MrtOpenCV_FIND_REQUIRED)
-	find_package(OpenCV REQUIRED)
-elseif(MrtOpenCV_FIND_QUIETLY)
-	find_package(OpenCV QUIET)
-else()
-	find_package(OpenCV)
-endif()
