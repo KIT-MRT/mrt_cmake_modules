e8e860fe538b3cc090f6da0a28e89782a1c73102
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2019-12-18 10:57:58 +0100

    Remove findscripts that are not referenced by cmake.yaml and unused

--- cmake/Modules/FindGTKMM.cmake
+++
@@ -1,6 +0,0 @@
-find_package(PkgConfig)
-
-pkg_check_modules(GTKMM gtkmm-3.0) # look into FindPkgConfig.cmake, 
-                                                                # it contains documentation
-# Now the variables GTKMM_INCLUDE_DIRS, GTKMM_LIBRARY_DIRS and GTKMM_LIBRARIES 
-# contain what you expect 
