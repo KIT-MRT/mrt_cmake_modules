8e1b04a37a64daade22b14cfb5046623732b978f
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2019-12-30 15:21:05 +0100

    Extend GatherDeps so that non-cmake export_depends from dependencies are considered as well
    Extend cmake.yaml to contain a few libraries commonly used as 'depend' by ros. Add functionality to add an empty cmake definition
    for such dependencies if they do not require being found by cmake (these should better use the exec_depend, but even ROS packages do not adhere to that)

--- scripts/generate_cmake_dependency_file.py
+++ scripts/generate_cmake_dependency_file.py
@@ -22,8 +22,15 @@ import platform
 import xml.etree.ElementTree as ET
 import yaml
 from catkin_pkg.packages import find_packages
-
-import sys
+from string import Template
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+if sys.version_info >= (3, 0):
+    import ast.literal_eval as eval_expr
+else:
+    eval_expr = eval # with python2 we have to just hope no one uses "rm -rf /" as condition in his package.xml...
 
 
 def eprint(*args, **kwargs):
@@ -42,6 +49,9 @@ class Dependency:
     build_depend = False
     build_export_depend = False
     test_depend = False
+
+    def isCatkin(self):
+        return self.packageType == PackageType.catkin
 
     def __repr__(self):
         return "Dependency(name:%s type:%s buld_depend:%s build_export_depend:%s test_depend:%s)" % (self.name, self.packageType, self.build_depend,
@@ -66,15 +76,18 @@ class PackageCMakeData:
     targets = list()
     """contains the targets which are defined when calling find_package(...)"""
 
-    def __init__(self, data):
+    def __init__(self, data={}):
         """Init cmake package data from a dict"""
-        self.name = data["name"]
+        self.name = data.get("name", "")
         self.includeDirs = data.get("include_dirs", list())
         self.libraryDirs = data.get("library_dirs", list())
         self.libraries = data.get("libraries", list())
         self.components = data.get("components", list())
         self.targets = data.get("targets", list())
         self.warning = data.get("warning", "")
+
+    def __nonzero__(self):
+        return bool(self.name)
 
     def __repr__(self):
         return "PackageCMakeData(name:" + self.name + " include_dirs:" + str(self.includeDirs) + " library_dirs:" + \
@@ -124,101 +137,57 @@ def readPackageCMakeData(rosDebYamlFileName):
     """
     # load ros dep yaml file
     f = open(rosDebYamlFileName, "r")
-    rosDebYamlData = yaml.load(f)
+    rosDebYamlData = yaml.load(f, Loader=Loader)
 
     # dictionary for storing cmake dependencies
     # e.g. { "<package name 1>" -> PackageCMakeData, "<package name 2>" -> PackageCMakeData ... }
     data = {}
+    distro = platform.dist()[2]
+    if 'ROS_OS_OVERRIDE' in os.environ:
+        ros_os_override = os.environ['ROS_OS_OVERRIDE'].split(':')
+        if len(ros_os_override) == 2:
+            distro = ros_os_override[1]
+
     for packageName, packageCMakeData in rosDebYamlData.items():
         # find out which distribution
-        distro = platform.dist()[2]
-        if 'ROS_OS_OVERRIDE' in os.environ:
-            ros_os_override = os.environ['ROS_OS_OVERRIDE'].split(':')
-            if len(ros_os_override) == 2:
-                distro = ros_os_override[1]
-
         if "name" in packageCMakeData:
             data[packageName] = PackageCMakeData(packageCMakeData)
         elif distro in packageCMakeData:
             data[packageName] = PackageCMakeData(packageCMakeData[distro])
+        elif not packageCMakeData:
+            data[packageName] = PackageCMakeData() # placeholder
     return data
 
 
-def getCatkinPackages(workspaceRoot):
-    """Get all available catkin packages"""
-    manifest = "package.xml"
-    catkin_ignore = "CATKIN_IGNORE"
-    nosubdirs = "rospack_nosubdirs"
-    ros_package_env = "ROS_PACKAGE_PATH"
-
-    def getPackagesInPath(packages, path):
-        for root, dirs, files in os.walk(path, topdown=True, followlinks=True):
-            if catkin_ignore in files:
-                dirs[:] = []  # instruct walk to not recurse deeper
-                continue
-            if manifest in files:  # found a package
-                package = ET.parse(os.path.join(root, manifest)).getroot()
-                export = package.find("export")
-                # ignore metapackages
-                if export is None or export.find("metapackage") is None:
-                    name = package.get("name", default=os.path.basename(root))
-                    packages.add(name)
-                dirs[:] = []
-                continue
-            if nosubdirs in files:
-                dirs[:] = []  # package is tagged to not recurse
-                continue
-
-    package_paths = set()
-    paths = os.environ.get(ros_package_env, "").split(":")
-    paths.append(workspaceRoot)
-    packages = set()
-    for path in paths:
-        getPackagesInPath(packages, path)
-    return list(packages)
-
-
-def main(packageXmlFile, rosDepYamlFileName, outputFile):
-    """
-    Reads a catkin package xml file, extract all dependencies and generate
-    a cmake which is used by FindDependendPackages.cmake to automatically
-    find_package catkin and non-catkin packages.
-
-    This script must be called in a ROS environment so that rospack find
-    all of catkin packages.
-
-    @param[in] packageXmlFile: catkin package xml file name
-    @param[in] rosDepYamlFileName: name of the ros dep yaml file which contains the
-                               cmake information for non-catkin packages
-    @param[out] outputFile: File name to the generated cmake file
-    """
-    # read package xml file
-    tree = ET.parse(packageXmlFile)
-
+def parseManifest(parsed_xml, catkin_packages, validate=True):
     # check catkin package xml file
-    root = tree.getroot()
-    if root.tag != 'package':
-        raise Exception("Cannot find package node in xml file")
-    if 'format' not in root.attrib:
-        raise Exception(
-            "Catkin package format must be 2. Change package.xml to <package format=\"2\">")
-    if root.attrib['format'] != "2":
-        raise Exception("Package format must be 2")
-
-    # get all catkin packages to distinguish between
-    # catkin and non-catkin packages
-    workspaceRoot = findWorkspaceRoot(os.path.abspath(packageXmlFile))
-    catkin_packages = getCatkinPackages(workspaceRoot)
-    # read rosdep yaml file for cmake variables used for non-catkin packages
-    # to automatically create a find_package(...)
-    cmakeVarData = readPackageCMakeData(rosDepYamlFileName)
+    if validate:
+        if parsed_xml.tag != 'package':
+            raise Exception("Cannot find package node in xml file")
+        if 'format' not in parsed_xml.attrib:
+            raise Exception(
+                "Catkin package format must be 2. Change package.xml to <package format=\"2\">")
+        if not (parsed_xml.attrib['format'] == "2" or parsed_xml.attrib['format'] == "3"):
+            raise Exception("Package format must be 2 or 3")
 
     # variable used to hold the Dependency classes from the package xml
     depends = []
     cuda_depends = []
 
-    for child in root:
+    for child in parsed_xml:
         depend = Dependency()
+
+        # check conditions
+        condition = child.get("condition")
+        if condition:
+            try:
+                is_fulfilled = eval_expr(Template(condition).substitute(os.environ))
+            except:
+                if validate:
+                    raise
+                is_fulfilled = True
+            if not is_fulfilled:
+                continue
 
         # check tag
         if child.tag == "depend":
@@ -241,6 +210,7 @@ def main(packageXmlFile, rosDepYamlFileName, outputFile):
             for export_child in child:
                 if export_child.tag == "cuda_depend":
                     cuda_depends.append(export_child.text)
+            continue
         else:
             continue
 
@@ -248,13 +218,91 @@ def main(packageXmlFile, rosDepYamlFileName, outputFile):
         depend.name = child.text
 
         # check if catkin package
-        if depend.name in catkin_packages:
-            depend.packageType = PackageType.catkin
-        else:
-            depend.packageType = PackageType.other
+        depend.packageType = PackageType.catkin if depend.name in catkin_packages else PackageType.other
 
         depends.append(depend)
+    return depends, cuda_depends
 
+
+def gatherDependeciesRecursive(manifest, catkin_packages, ignored_packages=set()):
+    first_pass = not ignored_packages
+    all_deps, cuda_depends = parseManifest(manifest, catkin_packages, validate=first_pass)
+    depends = []
+    # recursively gather the build_export_depends of these dependencies
+    for dep in all_deps:
+        if dep.name in ignored_packages:
+            continue
+        if not first_pass and not dep.build_export_depend:
+            continue
+        ignored_packages.add(dep.name)
+        if first_pass or not dep.isCatkin():
+            depends.append(dep) # catkin complains if we report catkin dependencies not mentioned in package.xml
+        if dep.packageType == PackageType.catkin:
+            depends += gatherDependeciesRecursive(catkin_packages[dep.name], catkin_packages, ignored_packages)[0]
+    return depends, cuda_depends
+
+
+def getCatkinPackages(workspaceRoot):
+    """Get all available catkin packages"""
+    manifest = "package.xml"
+    catkin_ignore = "CATKIN_IGNORE"
+    nosubdirs = "rospack_nosubdirs"
+    ros_package_env = "ROS_PACKAGE_PATH"
+
+    def getPackagesInPath(packages, path):
+        for root, dirs, files in os.walk(path, topdown=True, followlinks=True):
+            if catkin_ignore in files:
+                dirs[:] = []  # instruct walk to not recurse deeper
+                continue
+            if manifest in files:  # found a package
+                package = ET.parse(os.path.join(root, manifest)).getroot()
+                export = package.find("export")
+                # ignore metapackages
+                if export is None or export.find("metapackage") is None:
+                    name = package.findtext("name", default=os.path.basename(root))
+                    if not name in packages:
+                        packages[name] = package
+                dirs[:] = []
+                continue
+            if nosubdirs in files:
+                dirs[:] = []  # package is tagged to not recurse
+                continue
+
+    package_paths = {}
+    paths = os.environ.get(ros_package_env, "").split(":")
+    paths.append(workspaceRoot)
+    packages = {}
+    for path in paths:
+        getPackagesInPath(packages, path)
+    return packages
+
+
+def main(packageXmlFile, rosDepYamlFileName, outputFile):
+    """
+    Reads a catkin package xml file, extract all dependencies and generate
+    a cmake which is used by FindDependendPackages.cmake to automatically
+    find_package catkin and non-catkin packages.
+
+    This script must be called in a ROS environment so that rospack find
+    all of catkin packages.
+
+    @param[in] packageXmlFile: catkin package xml file name
+    @param[in] rosDepYamlFileName: name of the ros dep yaml file which contains the
+                               cmake information for non-catkin packages
+    @param[out] outputFile: File name to the generated cmake file
+    """
+    # read package xml file
+    tree = ET.parse(packageXmlFile).getroot()
+
+    # get all catkin packages to distinguish between
+    # catkin and non-catkin packages
+    workspaceRoot = findWorkspaceRoot(os.path.abspath(packageXmlFile))
+    catkin_packages = getCatkinPackages(workspaceRoot)
+    # read rosdep yaml file for cmake variables used for non-catkin packages
+    # to automatically create a find_package(...)
+    cmakeVarData = readPackageCMakeData(rosDepYamlFileName)
+
+    depends, cuda_depends = gatherDependeciesRecursive(tree, catkin_packages)
     # check CUDA depends and categorize them as either catkin or other package
     depends_names = {d.name for d in depends}
     cuda_catkin_depends = []
@@ -270,62 +318,32 @@ def main(packageXmlFile, rosDepYamlFileName, outputFile):
         else:
             cuda_other_depends.append(cuda_depend)
 
+    # output variables
+    out = {}
+    out["dependend_packages"] = " ".join(depend.name for depend in depends)
+    out["catkin_pkgs"] = " ".join(s.name  for s in depends if s.isCatkin() and s.build_depend)
+    out["catkin_exp_pkgs"] = " ".join(s.name  for s in depends if s.isCatkin() and s.build_export_depend)
+    out["catkin_test_pkgs"] = " ".join(s.name  for s in depends if s.isCatkin() and s.test_depend)
+    out["other_pkgs"] = " ".join(s.name  for s in depends if not s.isCatkin() and s.build_depend)
+    out["other_exp_pkgs"] = " ".join(s.name  for s in depends if not s.isCatkin() and s.build_export_depend)
+    out["other_test_pkgs"] = " ".join(s.name  for s in depends if not s.isCatkin() and s.test_depend)
+
     # generate output file
     f = open(outputFile, "w")
 
-    # write auto generate content
-    f.write("#This is an autogenerated file. Do not edit!\n")
-    f.write("#Changes will be overritten the next time cmake runs.\n")
-    f.write("\n")
+    text = ("#This is an autogenerated file. Do not edit!\n"
+            "#Changes will be overritten the next time cmake runs.\n"
+            "\n"
+            "set(DEPENDEND_PACKAGES $dependend_packages)\n"
+            "set(_CATKIN_PACKAGES_ $catkin_pkgs)\n"
+            "set(_CATKIN_EXPORT_PACKAGES_ $catkin_exp_pkgs)\n"
+            "set(_CATKIN_TEST_PACKAGES_ $catkin_test_pkgs)\n"
+            "set(_OTHER_PACKAGES_ $other_pkgs)\n"
+            "set(_OTHER_EXPORT_PACKAGES_ $other_exp_pkgs)\n"
+            "set(_OTHER_TEST_PACKAGES_ $other_test_pkgs)\n\n"
+            )
 
-    # write all dependend packages
-    packages = set()
-    for depend in depends:
-        packages.add(depend.name)
-
-    f.write("set(DEPENDEND_PACKAGES %s)\n" % ' '.join(packages))
-
-    # write catkin packages
-    packages = set()
-    for depend in (s for s in depends if s.packageType == PackageType.catkin and s.build_depend == True):
-        packages.add(depend.name)
-
-    f.write("set(_CATKIN_PACKAGES_ %s)\n" % ' '.join(packages))
-
-    # write catkin export packages
-    packages = set()
-    for depend in (s for s in depends if s.packageType == PackageType.catkin and s.build_export_depend == True):
-        packages.add(depend.name)
-
-    f.write("set(_CATKIN_EXPORT_PACKAGES_ %s)\n" % ' '.join(packages))
-
-    # write catkin test packages
-    packages = set()
-    for depend in (s for s in depends if s.packageType == PackageType.catkin and s.test_depend == True):
-        packages.add(depend.name)
-
-    f.write("set(_CATKIN_TEST_PACKAGES_ %s)\n" % ' '.join(packages))
-
-    # write non-catkin packages
-    packages = set()
-    for depend in (s for s in depends if s.packageType == PackageType.other and s.build_depend == True):
-        packages.add(depend.name)
-
-    f.write("set(_OTHER_PACKAGES_ %s)\n" % ' '.join(packages))
-
-    # write non-catkin export packages
-    packages = set()
-    for depend in (s for s in depends if s.packageType == PackageType.other and s.build_export_depend == True):
-        packages.add(depend.name)
-
-    f.write("set(_OTHER_EXPORT_PACKAGES_ %s)\n" % ' '.join(packages))
-
-    # write non-catkin test packages
-    packages = set()
-    for depend in (s for s in depends if s.packageType == PackageType.other and s.test_depend == True):
-        packages.add(depend.name)
-
-    f.write("set(_OTHER_TEST_PACKAGES_ %s)\n" % ' '.join(packages))
+    f.write(Template(text).substitute(out))
 
     # write CUDA catkin / other packages
     if cuda_catkin_depends:
@@ -338,6 +356,11 @@ def main(packageXmlFile, rosDepYamlFileName, outputFile):
     # write cmake variables (only those which are used in this package)
     for depend in (s for s in depends if s.name in cmakeVarData):
         cmakeData = cmakeVarData[depend.name]
+        if not cmakeData:
+            # package needs no cmake definition
+            f.write("set(_{}_NO_CMAKE_ 1)\n".format(depend.name))
+            continue
+
         f.write("set(_" + depend.name + "_CMAKE_NAME_ " + cmakeData.name + ")\n")
         if cmakeData.includeDirs:
             f.write("set(_" + depend.name + "_CMAKE_INCLUDE_DIRS_ " +
@@ -359,8 +382,4 @@ def main(packageXmlFile, rosDepYamlFileName, outputFile):
 
 
 if __name__ == "__main__":
-    try:
-        main(sys.argv[1], sys.argv[2], sys.argv[3])
-    except BaseException as e:
-        eprint(str(e))
-        sys.exit(1)
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
