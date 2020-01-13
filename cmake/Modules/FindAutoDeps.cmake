a63d5fe4f96d28a0507589cf0331afeccf9e9260
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2020-01-13 21:42:29 +0100

    Refactor everything to use targets instead of passing flags and stuff around
    This also includes refactoring of AutoDeps, that now generates targets and adds the dependecies also as targets to this dependency target

--- cmake/Modules/FindAutoDeps.cmake
+++ cmake/Modules/FindAutoDeps.cmake
@@ -1,4 +1,4 @@
-#author: Johannes Beck
+#author: Johannes Beck, Fabian Poggenhans
 #email: Johannes.Beck@kit.edu
 #license: GPLv3
 #
@@ -7,38 +7,29 @@
 #
 #
 #Variables used from the generate_cmake_depencency_file.py:
+#DEPENDEND_PACKAGES: all packages mentioned in package.xml
 #_CATKIN_PACKAGES_: contains all catkin packages
-#_CATKIN_EXPORT_PACKAGES_: contains only the catkin packages, which shall be exported
-#
-#_OTHER_PACKAGES_: contains all other packages
-#_OTHER_EXPORT_PACKAGES_: contains only the mrt packages, which shall be exported
-#_CUDA_CATKIN_PACKAGES_: contains all packages which should be linked to cuda
-#_CUDA_OTHER_PACKAGES_: contains all packages which should be linked to cuda
+#_MRT_PACKAGES_: contains all packages that are build_depends
+#_MRT_EXPORT_PACKAGES_: contains all packages that are build_export_depends
+#_MRT_CUDA_PACKAGES_: contains all packages which should be linked to cuda
 #
 #_<package name>_CMAKE_INCLUDE_DIRS_: contains the find package variable include cmake name
 #_<package name>_CMAKE_LIBRARY_DIRS_: contains the find package variable libraries cmake name
 #_<package name>_CMAKE_LIBRARIES_: contains the find package variable libraries cmake name
 #_<package name>_CMAKE_COMPONENTS_: components used in find_package(...) for the package
 #
-#Variables set by this script:
-#catkin_DEPENDS: Contains all catkin packages
-#catkin_EXPORT_DEPENDS: Contains all catkin packages, which shall be exported. This shall be used in catkin_package
+# Sets the following targets:
+# <prefix>auto_deps: A target that contains all targets that should be linked against internally
+# <prefix>auto_deps_cuda: A target that contains all targets which needs to be linked into cuda code.
+# <prefix>auto_deps_export: A target that contains all libraries which need to be linked publically
 #
-#mrt_INCLUDE_DIRS: Contains all include directories used for building the package
-#mrt_LIBRARY_DIRS: Contains all library directories used for building the package
-#mrt_LIBRARIES: Contains all libraries used for building the package
-#mrt_CUDA_LIBRARIES: Contains all libraries which needs to be linked into cuda code.
-#mrt_TEST_LIBRARIES: Contains all libraries which needs to be linked to test executables.
-#mrt_EXPORT_INCLUDE_DIRS: Contains all include directories which dependend packages also need for building
-#mrt_EXPORT_LIBRARIES: Contains all libraries which dependend packages also need for building
+# The prefix will be chosen based on the value of the variable AutoDeps_PREFIX if set.
+#
+# Also creates a target that has the name component::component for each component passed as an input unless the component itself already defines targets.
 
-set(mrt_INCLUDE_DIRS "")
-set(mrt_EXPORT_INCLUDE_DIRS "")
-set(mrt_LIBRARIES "")
-set(mrt_EXPORT_LIBRARIES "")
-set(mrt_CUDA_LIBRARIES "")
-set(mrt_TEST_LIBRARIES "")
-set(mrt_LIBRARY_DIRS "")
+add_library(${AutoDeps_PREFIX}auto_deps INTERFACE IMPORTED)
+add_library(${AutoDeps_PREFIX}auto_deps_cuda INTERFACE IMPORTED)
+add_library(${AutoDeps_PREFIX}auto_deps_export INTERFACE IMPORTED)
 
 function(_cleanup_includes var_name_include_dir)
     if(${var_name_include_dir})
@@ -51,6 +42,83 @@ function(_cleanup_includes var_name_include_dir)
     set (${var_name_include_dir} ${${var_name_include_dir}} PARENT_SCOPE)
 endfunction()
 
+function(_add_system_includes targetname)
+    list(FIND ARGN "/opt/mrtsoftware/local/include" _FOUND_LOCAL)
+    list(FIND ARGN "/opt/mrtsoftware/release/include" _FOUND_RELEASE)
+    if(NOT ${_FOUND_LOCAL} EQUAL -1 OR NOT ${_FOUND_RELEASE} EQUAL -1)
+        set_target_properties(
+            ${targetname}
+            INTERFACE_SYSTEM_INCLUDE_DIRECTORIES /opt/mrtsoftware/local/include;/opt/mrtsoftware/release/include
+            )
+    endif()
+endfunction()
+
+macro(_find_target output_target component)
+    set(_targetname ${component}::${component})
+    list(FIND _CATKIN_PACKAGES_ ${component} _is_catkin_package)
+    message(STATUS "Finding target ${component}")
+    if(TARGET ${_targetname})
+        target_link_libraries(${output_target} INTERFACE ${_targetname})
+        message("--> Already exists")
+    elseif(NOT ${_is_catkin_package} EQUAL -1)
+        # is catkin package
+        find_package(${component} REQUIRED)
+        if(NOT TARGET ${_targetname})
+            # we have to create a new target. catkin does not create them.
+            add_library(${_targetname} INTERFACE IMPORTED)
+            set_target_properties(${_targetname} PROPERTIES
+                INTERFACE_INCLUDE_DIRECTORIES "${${component}_INCLUDE_DIRS}"
+                INTERFACE_LINK_DIRECTORIES "${${component}_LIBRARY_DIRS}"
+                INTERFACE_LINK_LIBRARIES "${${component}_LIBRARIES}"
+                )
+            if(${component}_EXPORTED_TARGETS)
+                add_dependencies(${_targetname} ${${component}_EXPORTED_TARGETS})
+            endif()
+            _add_system_includes(${_targetname} ${${component}_INCLUDE_DIRS})
+        endif()
+        target_link_libraries(${output_target} INTERFACE ${_targetname})
+        message(STATUS "--> Is catkin package with include: ${${component}_INCLUDE_DIRS}, lib: ${${component}_LIBRARIES}")
+    else()
+        # its an external package
+        if(_${component}_NO_CMAKE_)
+            # package is known to set no variables. do nothing.
+        elseif(NOT DEFINED _${component}_CMAKE_NAME_)
+            message(FATAL_ERROR "Package ${component} is specified for autodepend but no cmake definition was found. Did you resolve dependencies?")
+        else()
+            #find non-catkin modules
+            if(DEFINED _${component}_CMAKE_COMPONENTS_)
+                find_package(${_${component}_CMAKE_NAME_} REQUIRED COMPONENTS ${_${component}_CMAKE_COMPONENTS_})
+            else()
+                find_package(${_${component}_CMAKE_NAME_} REQUIRED)
+            endif()
+
+            # add them to auto_deps target
+            if(DEFINED _${component}_CMAKE_TARGETS_)
+                # the library already defines a target for us. Everything is good.
+                target_link_libraries(${output_target} INTERFACE ${_${component}_CMAKE_TARGETS_})
+                message(STATUS "--> Is externally defined target: ${_${component}_CMAKE_TARGETS_}")
+            else()
+                # the library defines no target. Create it from the variables it sets.
+                add_library(${_targetname} INTERFACE IMPORTED)
+                set(_includes ${${_${component}_CMAKE_INCLUDE_DIRS_}})
+                _cleanup_includes(_includes)
+                set_target_properties(${_targetname} PROPERTIES
+                    INTERFACE_INCLUDE_DIRECTORIES "${_includes}"
+                    INTERFACE_LINK_DIRECTORIES "${${_${component}_CMAKE_LIBRARY_DIRS_}}"
+                    INTERFACE_LINK_LIBRARIES "${${_${component}_CMAKE_LIBRARIES_}}"
+                    )
+                _add_system_includes(${_targetname} ${includes})
+                unset(_includes)
+                target_link_libraries(${output_target} INTERFACE ${_targetname})
+                message(STATUS "--> Is externally defined variables: include: ${_includes}, libs: ${${_${component}_CMAKE_LIBRARIES_}}")
+            endif() # package defines targets
+        endif() # package definition is valid
+    endif() # catkin vs normal package
+    # cleanup
+    unset(_targetname)
+    unset(_is_catkin_package)
+endmacro()
+
 # Detect Conan builds. In this case we don't have to do anything, just load the conanfile and include the catkin mock.
 if(CONAN_PACKAGE_NAME OR EXISTS ${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
     if(NOT CONAN_PACKAGE_NAME)
@@ -61,161 +129,59 @@ if(CONAN_PACKAGE_NAME OR EXISTS ${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 endif()
 
 if (AutoDeps_FIND_COMPONENTS)
-    #extract packages packages
-    set(_CATKIN_SELECTED_PACKAGES_ "")
-    set(_OTHER_SELECTED_PACKAGES_ "")
-    set(_CATKIN_TEST_SELECTED_PACKAGES_ "")
-    set(_OTHER_TEST_SELECTED_PACKAGES_ "")
-
-    foreach(component ${AutoDeps_FIND_COMPONENTS})
-        list(FIND _CATKIN_PACKAGES_ ${component} res)
-        if(NOT ${res} EQUAL -1)
-            list(APPEND _CATKIN_SELECTED_PACKAGES_ ${component})
-            continue()
-        endif()
-
-        list(FIND _OTHER_PACKAGES_ ${component} res)
-        if(NOT ${res} EQUAL -1)
-            list(APPEND _OTHER_SELECTED_PACKAGES_ ${component})
-            continue()
-        endif()
-
-        list(FIND _CATKIN_TEST_PACKAGES_ ${component} res)
-        if(NOT ${res} EQUAL -1)
-            list(APPEND _CATKIN_TEST_SELECTED_PACKAGES_ ${component})
-            continue()
-        endif()
-
-        list(FIND _OTHER_TEST_PACKAGES_ ${component} res)
-        if(NOT ${res} EQUAL -1)
-            list(APPEND _OTHER_TEST_SELECTED_PACKAGES_ ${component})
-            continue()
-        endif()
-
-        message(SEND_ERROR "Package ${component} specified but not found in package.xml. This package is ignored.")
-    endforeach()
-    
-    if(CATKIN_ENABLE_TESTING)
-        list(APPEND _CATKIN_SELECTED_PACKAGES_ ${_CATKIN_TEST_SELECTED_PACKAGES_})
-        list(APPEND _OTHER_SELECTED_PACKAGES_ ${_OTHER_TEST_SELECTED_PACKAGES_})
-    endif()
-    
-    #find catkin packages
-    find_package(catkin REQUIRED COMPONENTS ${_CATKIN_SELECTED_PACKAGES_})
-
-    #append catkin packages libraries to CUDA libraries
-    foreach(cuda_package ${_CUDA_CATKIN_PACKAGES_})
-        list(APPEND mrt_CUDA_LIBRARIES ${${cuda_package}_LIBRARIES})
-    endforeach()
-    
-    #find other packages
-    foreach(other_package ${_OTHER_SELECTED_PACKAGES_})
-        #check, if cmake variable mapping is available
-        if(_${other_package}_NO_CMAKE_)
-            continue() # package is known to set no variables
-        endif()
-        if(NOT DEFINED _${other_package}_CMAKE_NAME_)
-            message(FATAL_ERROR "Package ${other_package} is specified for autodepend but no cmake definition was found. Did you resolve dependencies?")
-        endif()
-        
-        #find non catkin modules
-        if(DEFINED _${other_package}_CMAKE_COMPONENTS_)
-            find_package(${_${other_package}_CMAKE_NAME_} REQUIRED COMPONENTS ${_${other_package}_CMAKE_COMPONENTS_})
+    foreach(_component ${AutoDeps_FIND_COMPONENTS})
+        # figure out where this variable goes
+        list(FIND _MRT_PACKAGES_ ${_component} _is_package)
+        list(FIND _MRT_EXPORT_PACKAGES_ ${_component} _is_export_package)
+        list(FIND _MRT_TEST_PACKAGES_ ${_component} _is_test_package)
+        list(FIND _MRT_CUDA_PACKAGES_ ${_component} _is_cuda_package)
+        list(FIND _CATKIN_PACKAGES_ ${_component} _is_catkin_package)
+        if(NOT ${_is_export_package} EQUAL -1)
+            _find_target(${AutoDeps_PREFIX}auto_deps_export ${_component})
+        elseif(NOT ${_is_package} EQUAL -1)
+            _find_target(${AutoDeps_PREFIX}auto_deps ${_component})
+        elseif(NOT ${_is_test_package} EQUAL -1)
+            if(CATKIN_ENABLE_TESTING)
+                _find_target(${AutoDeps_PREFIX}auto_deps ${_component})
+            endif()
         else()
-            find_package(${_${other_package}_CMAKE_NAME_} REQUIRED)
+            message(SEND_ERROR "Package ${_component} specified but not found in package.xml. This package is ignored.")
         endif()
-        
-        #append found include directories
-        if(DEFINED _${other_package}_CMAKE_INCLUDE_DIRS_)
-            if(NOT DEFINED ${_${other_package}_CMAKE_INCLUDE_DIRS_})
-                message(FATAL_ERROR "Package ${other_package}: Specified include dir variable ${_${other_package}_CMAKE_INCLUDE_DIRS_} not set.")
-            endif()
-        
-            list(APPEND mrt_INCLUDE_DIRS ${${_${other_package}_CMAKE_INCLUDE_DIRS_}})
-            
-            list(FIND _OTHER_EXPORT_PACKAGES_ ${other_package} res)
-            if(NOT ${res} EQUAL -1)
-                list(APPEND mrt_EXPORT_INCLUDE_DIRS ${${_${other_package}_CMAKE_INCLUDE_DIRS_}})
-            endif()
+        if(NOT ${is_cuda_package} EQUAL -1)
+            _find_target(${AutoDeps_PREFIX}auto_deps_cuda ${_component})
         endif()
-        
-        #append library directories
-        if(DEFINED _${other_package}_CMAKE_LIBRARY_DIRS_)
-            if(NOT DEFINED ${_${other_package}_CMAKE_LIBRARY_DIRS_})
-                message(FATAL_ERROR "Package ${other_package}: Specified library dirs variable ${${_${other_package}_CMAKE_LIBRARY_DIRS_}} not set.")
-            endif()
-            
-            list(APPEND mrt_LIBRARY_DIRS ${${_${other_package}_CMAKE_LIBRARY_DIRS_}})
-        endif()
-        
-        #append libraries
-        if(DEFINED _${other_package}_CMAKE_LIBRARIES_)
-            if(NOT DEFINED ${_${other_package}_CMAKE_LIBRARIES_})
-                message(FATAL_ERROR "Package ${other_package}: Specified libraries variable ${${_${other_package}_CMAKE_LIBRARIES_}} not set.")
-            endif()
+        unset(_is_package)
+        unset(_is_export_package)
+        unset(_is_test_package)
+        unset(_is_cuda_package)
+        unset(_is_catkin_package)
+    endforeach()
 
-            # Append all libraries to link against a test executable (regular and test only).
-            list(APPEND mrt_TEST_LIBRARIES ${${_${other_package}_CMAKE_LIBRARIES_}})
-
-            list(FIND _OTHER_PACKAGES_ ${other_package} res)
-            if(NOT ${res} EQUAL -1)
-                list(APPEND mrt_LIBRARIES ${${_${other_package}_CMAKE_LIBRARIES_}})
+    # set the variables as expected by catkin_package (for backwards compability)
+    set(mrt_EXPORT_INCLUDE_DIRS "")
+    set(mrt_EXPORT_LIBRARIES "")
+    set(catkin_EXPORT_DEPENDS ${_CATKIN_SELECTED_PACKAGES_})
+    get_target_property(_export_targets ${AutoDeps_PREFIX}auto_deps_export INTERFACE_LINK_LIBRARIES)
+    foreach(_target ${_export_targets})
+        get_target_property(_target_type ${_target} TYPE)
+        if(NOT ${_target_type} STREQUAL "INTERFACE_LIBRARY")
+            list(APPEND mrt_EXPORT_LIBRARIES ${_target})
+        else()
+            get_target_property(_target_include ${_target} INTERFACE_INCLUDE_DIRECTORIES)
+            get_target_property(_target_link ${_target} INTERFACE_LINK_LIBRARIES)
+            if(_target_include)
+                list(APPEND mrt_EXPORT_INCLUDE_DIRS ${_target_include})
             endif()
-
-            list(FIND _OTHER_EXPORT_PACKAGES_ ${other_package} res)
-            if(NOT ${res} EQUAL -1)
-                list(APPEND mrt_EXPORT_LIBRARIES ${${_${other_package}_CMAKE_LIBRARIES_}})
-            endif()
-
-            list(FIND _CUDA_OTHER_PACKAGES_ ${other_package} res)
-            if(NOT ${res} EQUAL -1)
-                list(APPEND mrt_CUDA_LIBRARIES ${${_${other_package}_CMAKE_LIBRARIES_}})
-            endif()
-        endif()
-        # append targets
-        if(DEFINED _${other_package}_CMAKE_TARGETS_)
-            # Append all libraries to link against a test executable (regular and test only).
-            list(APPEND mrt_TEST_LIBRARIES ${_${other_package}_CMAKE_TARGETS_})
-
-            list(FIND _OTHER_PACKAGES_ ${other_package} res)
-            if(NOT ${res} EQUAL -1)
-                list(APPEND mrt_LIBRARIES ${_${other_package}_CMAKE_TARGETS_})
-            endif()
-
-            list(FIND _OTHER_EXPORT_PACKAGES_ ${other_package} res)
-            if(NOT ${res} EQUAL -1)
-                list(APPEND mrt_EXPORT_LIBRARIES ${_${other_package}_CMAKE_TARGETS_})
-            endif()
-
-            list(FIND _CUDA_OTHER_PACKAGES_ ${other_package} res)
-            if(NOT ${res} EQUAL -1)
-                list(APPEND mrt_CUDA_LIBRARIES ${_${other_package}_CMAKE_TARGETS_})
+            if(_target_link)
+                list(APPEND mrt_EXPORT_LIBRARIES ${_target_link})
             endif()
         endif()
     endforeach()
-        
-    # Cleanup include directories.
-    _cleanup_includes(mrt_INCLUDE_DIRS)
-    _cleanup_includes(mrt_EXPORT_INCLUDE_DIRS)
-    _cleanup_includes(catkin_INCLUDE_DIRS)
 
-    # Mark '/opt/mrtsoftware/...' include path as system headers, otherwise
-    # the order can get messed up between 'local' and 'release'.
-    set(_ALL_INCLUDE_DIRS ${mrt_INCLUDE_DIRS} ${catkin_INCLUDE_DIRS})
-    if(_ALL_INCLUDE_DIRS)
-        list(FIND _ALL_INCLUDE_DIRS "/opt/mrtsoftware/local/include" _FOUND_LOCAL)
-        list(FIND _ALL_INCLUDE_DIRS "/opt/mrtsoftware/release/include" _FOUND_RELEASE)
-
-        if (NOT ${_FOUND_LOCAL} EQUAL -1 OR NOT ${_FOUND_RELEASE} EQUAL -1)
-            include_directories(SYSTEM "/opt/mrtsoftware/local/include" "/opt/mrtsoftware/release/include")
-        endif()
-    endif()
-
-    #remove -lpthread from exports as this will not work with the catkin find package script.
-    if(mrt_EXPORT_LIBRARIES)
-        list(REMOVE_ITEM mrt_EXPORT_LIBRARIES "-lpthread")
-    endif()
-
+    # cleanup and report
+    unset(_target_include)
+    unset(_target_link)
+    unset(_CATKIN_SELECTED_PACKAGES_)
+    message(STATUS "Includes: ${mrt_EXPORT_INCLUDE_DIRS}")
+    message(STATUS "Libs: ${mrt_EXPORT_LIBRARIES}")
 endif()
-
-set(catkin_EXPORT_DEPENDS ${_CATKIN_EXPORT_PACKAGES_})
