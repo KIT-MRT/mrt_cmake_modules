edd2fd7185aef3abf5cbb7dd113d5176c3b5adce
Piotr Orzechowski <piotr.orzechowski@kit.edu>
2020-11-25 11:45:25 +0100

    Format FindBLASFEO.cmake

--- cmake/Modules/FindBLASFEO.cmake
+++ cmake/Modules/FindBLASFEO.cmake
@@ -1,31 +1,28 @@
 message(STATUS "Looking for blasfeo")
 
-find_path(BLASFEO_INCLUDE_DIR
-    blasfeo_common.h
-  HINTS $ENV{BLASFEO}/include  /opt/blasfeo/include
-  )
+find_path(BLASFEO_INCLUDE_DIR blasfeo_common.h HINTS $ENV{BLASFEO}/include /opt/blasfeo/include)
 
 if(BLASFEO_INCLUDE_DIR)
-  message(STATUS "Found blasfeo include directory: ${BLASFEO_INCLUDE_DIR}")
+    message(STATUS "Found blasfeo include directory: ${BLASFEO_INCLUDE_DIR}")
 else()
-  message(STATUS "Could not find blasfeo include dir")
+    message(STATUS "Could not find blasfeo include dir")
 endif()
 
-find_library(BLASFEO_LIB
+find_library(
+    BLASFEO_LIB
     NAMES blasfeo
-    HINTS $ENV{BLASFEO}/lib /opt/blasfeo/lib
-)
+    HINTS $ENV{BLASFEO}/lib /opt/blasfeo/lib)
 
 if(BLASFEO_LIB)
-  set(BLASFEO_LIBRARIES ${BLASFEO_LIB})
-  message(STATUS "Found blasfeo libraries ${BLASFEO_LIBRARIES}")
-  set(BLASFEO_FOUND_LIBS TRUE)
+    set(BLASFEO_LIBRARIES ${BLASFEO_LIB})
+    message(STATUS "Found blasfeo libraries ${BLASFEO_LIBRARIES}")
+    set(BLASFEO_FOUND_LIBS TRUE)
 else()
-  message(STATUS "Could not find blasfeo libraries")
+    message(STATUS "Could not find blasfeo libraries")
 endif()
 
 if(BLASFEO_INCLUDE_DIR AND BLASFEO_FOUND_LIBS)
-  set(BLASFEO_FOUND TRUE)
+    set(BLASFEO_FOUND TRUE)
 else()
-  message(STATUS "BLASFEO: Cound not find blasfeo. Try setting BLASFEO env var.")
+    message(STATUS "BLASFEO: Cound not find blasfeo. Try setting BLASFEO env var.")
 endif()
