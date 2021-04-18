3eab34482dca86759afb74ba6a23a576a9e3ded6
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2021-04-18 17:52:05 +0200

    Headers from dependencies are no longer marked as system, except from overlayed workspaces

--- cmake/mrt_cmake_modules-macros.cmake
+++ cmake/mrt_cmake_modules-macros.cmake
@@ -21,6 +21,10 @@ if(DEFINED MRT_CLANG_TIDY_FLAGS)
 endif()
 unset(MRT_CLANG_TIDY_FLAGS)
 
+# Disable that all dependant packages are treated as "system" packages (suppressing all warnings from there).
+# We later add the "system" explicitly to all include dirs of parent workspaces.
+set(CMAKE_NO_SYSTEM_FROM_IMPORTED YES)
+
 # construct the mrt_sanitizer_lib_flags and  mrt_sanitizer_exe_flags target based on the configuation in the MRT_SANITIZER variable
 if(NOT TARGET ${PROJECT_NAME}_sanitizer_lib_flags AND NOT TARGET ${PROJECT_NAME}_sanitizer_exe_flags)
     add_library(${PROJECT_NAME}_sanitizer_lib_flags INTERFACE)
@@ -106,6 +110,32 @@ set(${PROJECT_NAME}_LOCAL_INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/include;${CA
 set(${PROJECT_NAME}_PYTHON_API_TARGET "") # contains the list of python api targets
 set(${PROJECT_NAME}_GENERATED_LIBRARIES "") # the list of library targets built and installed by the tools
 set(${PROJECT_NAME}_MRT_TARGETS "") # list of all public installable targets created by the functions here
+
+# this function determines all includes directories that belong to parent workspaces (including the ros installation).
+# we later mark all of these directories as system directories so that warnings from these headers are hidden.
+# It is important to maintain the order of CMAKE_INSTALL_PREFIX so that the include order matches the order in which workspaces are overlayed.
+function(_get_parent_workspace_include_dirs outdir)
+    # Assuming the parent directory of the install dir is the workspace.
+    # Not the best assumption but it works (in the worst case we mark too much headers as "system")
+    get_filename_component(current_workspace ${CMAKE_INSTALL_PREFIX} DIRECTORY)
+
+    set(workspace_include_dirs)
+    foreach(workspace ${CMAKE_PREFIX_PATH})
+        string(FIND ${workspace} ${current_workspace} _is_current_workspace)
+        if(NOT ${_is_current_workspace} EQUAL -1)
+            continue()
+        endif()
+        foreach(_include_dir ${mrt_EXPORT_INCLUDE_DIRS})
+            string(FIND ${_include_dir} ${workspace} _pos)
+            if(NOT ${_pos} EQUAL -1)
+                list(APPEND workspace_include_dirs ${_include_dir})
+            endif()
+        endforeach()
+    endforeach()
+    set(${outdir}
+        ${workspace_include_dirs}
+        PARENT_SCOPE)
+endfunction()
 
 # define rosparam/rosinterface_handler macro for compability. Macros will be overriden by the actual macros defined by the packages, if existing.
 macro(generate_ros_parameter_files)
@@ -277,6 +307,11 @@ function(mrt_add_links target)
                 target_include_directories(${target} PUBLIC $<BUILD_INTERFACE:${include}>)
             endif()
         endforeach()
+        # mark all include include dirs from parent workspaces as "system" (silence warnings from these)
+        _get_parent_workspace_include_dirs(workspace_include_dirs)
+        if(workspace_include_dirs)
+            target_include_directories(${target} SYSTEM AFTER PRIVATE ${workspace_include_dirs})
+        endif()
     else()
         # set the include dir for installation and dependent targets.
         foreach(include ${${PROJECT_NAME}_LOCAL_INCLUDE_DIRS})
